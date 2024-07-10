31e600faf7a5e1a3b227ed3757b1ebe1003fa693
YILDIZ TUNAR <ozdemirryildzz@gmail.com>
2024-07-10 17:17:42 +0400

    tailwind installed

--- myresume/src/components/Language.jsx
+++ myresume/src/components/Language.jsx
@@ -4,14 +4,14 @@ import { useTranslation } from 'react-i18next';
 export default function Language() {
   const { t, i18n } = useTranslation();
 
-  const clickHandle = () => {
+  const changeLanguage = () => {
     const newLanguage = i18n.language === 'tr' ? 'en' : 'tr';
     i18n.changeLanguage(newLanguage);
   };
 
   return (
     <div>
-      <button onClick={clickHandle}>
+      <button onClick={changeLanguage}>
         {i18n.language === 'tr' ? 'İNGİLİZCE’YE GEÇ' : 'TÜRKÇE’YE GEÇ'}
       </button>
       Aktif dil: {i18n.language}
