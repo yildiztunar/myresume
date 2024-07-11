98634fc07d86d84943c96b0bcd54cb08c5c638a8
YILDIZ TUNAR <ozdemirryildzz@gmail.com>
2024-07-11 14:51:55 +0400

    tailwindcss added

--- myresume/tailwind.config.js
+++ myresume/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
 
