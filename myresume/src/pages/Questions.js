823924d0444beadb9ca1cea2326d07c95282493c
YILDIZ TUNAR <ozdemirryildzz@gmail.com>
2024-07-15 21:34:02 +0400

    page renamed

---
+++ myresume/src/pages/Questions.js
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+
+function Questions() {
+  
+  return (
+    <>
+    <div>
+      <Header/>
+      <Projects/>
+      <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default Questions
