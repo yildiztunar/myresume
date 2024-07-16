714349a4bb765c4089421cd72cd401fa1f758388
YILDIZ TUNAR <ozdemirryildzz@gmail.com>
2024-07-16 12:07:07 +0400

    Resume added

---
+++ myresume/src/components/MyResume.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+
+
+function MyResume() {
+  
+  return (
+    <>
+    <div>
+      
+      
+    </div>
+    </>
+  )
+}
+
+export default MyResume
---
+++ myresume/src/pages/Resume.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import MyResume from '../components/MyResume'
+
+
+function Resume() {
+  
+  return (
+    <>
+    <div>
+      <Header/>
+      <MyResume/>
+    </div>
+    </>
+  )
+}
+
+export default Resume
